50ff0ebe7ca0fc44abff07b14fc720112b0b1d5a
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2024-07-04 01:14:15 +0530

    FEAT: Update the Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,50 +2,49 @@ import {useState, useEffect} from "react";
 import {Navbar, Container } from 'react-bootstrap';
 
 export const NavBar = () => {
-  const{activeLinks, setActiveLinks} = useState('home');
-  const {scolled, seScrolled} = useState(false);  
-
-  useEffect(() => {
-    const onScroll = () => {
-        if (window.scrollY > 50) {
-            seScrolled(true);
-        } else {
-            seScrolled(false);
-        }
-    }      
-
-    window.addEventListener('scroll', onScroll);
-
-    return () => window.removeEventListener('scroll', onScroll);
-
-  }, []);
-
-
-  return (
-    <Navbar expand="lg" className={scrolled ? "scrolled":""}>
-        <Container>
-            <Navbar.Brand href="#home">
-                <img src={''} alt="logo"/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav">
-                <span className="navbar-toggler-icon"></span>
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#skill">Skills</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-                <div className='social-icon'>
-                    <a href='#'><img src={} alt=''/></a>
-                    <a href='#'><img src={} alt=''/></a>
-                    <a href='#'><img src={} alt=''/></a>
-                </div>
-                <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect<span/><button/>
-            </span>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
-  )
+    const{activeLink, setActiveLink} = useState('home');
+    const {scolled, seScrolled} = useState(false);  
+  
+    useEffect(() => {
+      const onScroll = () => {
+          if (window.scrollY > 50) {
+              seScrolled(true);
+          } else {
+              seScrolled(false);
+          }
+      }      
+  
+      window.addEventListener('scroll', onScroll);
+  
+      return () => window.removeEventListener('scroll', onScroll);
+  
+    }, []);
+  
+    return (
+      <Navbar expand="lg" className={scrolled ? "scrolled":""}>
+          <Container>
+              <Navbar.Brand href="#home">
+                  <img src={''} alt="logo"/>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav">
+                  <span className="navbar-toggler-icon"></span>
+              </Navbar.Toggle>
+              <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                  <Nav.Link href="#home" className={activeLink}>Home</Nav.Link>
+                  <Nav.Link href="#skill">Skills</Nav.Link>
+                  <Nav.Link href="#projects">Projects</Nav.Link>
+              </Nav>
+              <span className="navbar-text">
+                  <div className='social-icon'>
+                      <a href='#'><img src={} alt=''/></a>
+                      <a href='#'><img src={} alt=''/></a>
+                      <a href='#'><img src={} alt=''/></a>
+                  </div>
+                  <button className='vvd' onClick={() => console.log('connect')}> <span>Let's Connect</span></button>
+              </span>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
+    )  
 }
